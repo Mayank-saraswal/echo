7696c24596a8efc947c3aba191563a9eb14259bb
Mayank Saini <mayanksaraswal@gmail.com>
2025-10-04 09:24:55 +0530

    add clerk auth

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { Authenticated, Unauthenticated } from "convex/react"
+import { SignInButton, UserButton } from "@clerk/nextjs"
 
 import { useMutation, useQuery } from "convex/react"
 import { api } from "@workspace/backend/_generated/api"
@@ -11,16 +13,32 @@ export default function Page() {
   const users = useQuery(api.users.getMany)
   const addUser = useMutation(api.users.add)
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <p>
-        app/web
-      </p>
-      <Button onClick={() => addUser()}>Add User</Button>
-      <div className="max-w-sm w-full mx-auto gap">
-      {JSON.stringify(users)}
+    <>
 
-      </div>
-   
-    </div>
+      <Authenticated>
+
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <p>
+            app/web
+          </p>
+          <UserButton />
+          
+          <Button onClick={() => addUser()}>Add User</Button>
+          <div className="max-w-sm w-full mx-auto gap">
+            {JSON.stringify(users)}
+
+          </div>
+
+        </div>
+      </Authenticated>
+
+      <Unauthenticated>
+        <p>MUst be signed in </p>
+        <SignInButton> Sign IN </SignInButton>
+        
+      </Unauthenticated>
+    </>
   )
+
+
 }
---
+++ apps/web/app/test/page.tsx
@@ -0,0 +1,10 @@
+const Page =()=>{
+    return(
+        <div>
+            Test Page !
+        </div>
+    )
+}
+
+
+export default Page 
